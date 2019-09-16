c0937f884e316ce8ce17fc5a6eac870e98ce3f45
Daniel Kaufmann <daniel.kaufmann96@gmx.at>
2019-09-16 08:20:39 +0200

    [FEATURE] style and add floating buttons

---
+++ components/DarkModeButton.vue
@@ -0,0 +1,22 @@
+<template
+  ><BaseButton
+    :fab="true"
+    :dark-mode="darkMode"
+    :icon="iconComputed"
+    @click="$emit('darkModeSet', !darkMode)"
+  ></BaseButton>
+</template>
+<script>
+import BaseButton from "./BaseButton";
+export default {
+  components: {
+    BaseButton
+  },
+  extends: BaseButton,
+  computed: {
+    iconComputed() {
+      return this.darkMode ? "mdi-weather-sunny" : "mdi-weather-night";
+    }
+  }
+};
+</script>
---
+++ components/SocialButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <BaseButton
+    :fab="true"
+    :dark-mode="darkMode"
+    :icon="`mdi-${type}`"
+    :href="link"
+  ></BaseButton>
+</template>
+<script>
+import BaseButton from "./BaseButton";
+export default {
+  components: {
+    BaseButton
+  },
+  extends: BaseButton,
+  props: {
+    type: {
+      type: String,
+      default: null
+    },
+    link: {
+      type: String,
+      default: null
+    }
+  }
+};
+</script>
