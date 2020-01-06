9275219e65387867eb0934ad622a5b59a0b1cc17
Daniel Kaufmann <daniel.kaufmann96@gmx.at>
2020-01-06 11:01:20 +0100

    [BUGFIX] fix button spacing

--- components/BaseButton.vue
+++ components/BaseButton.vue
@@ -1,8 +1,28 @@
-<template v-slot:activator
-  ><v-icon :name="name" :dark="darkMode" :href="href" @click="$emit('click')">
+<template v-slot:activator>
+  <a v-if="link" :href="link" class="icon">
+    <v-icon :name="name" :dark="darkMode" @click="$emit('click')">
+      {{ icon }}
+    </v-icon>
+  </a>
+  <v-icon
+    v-else
+    class="icon"
+    :name="name"
+    :dark="darkMode"
+    @click="$emit('click')"
+  >
     {{ icon }}
   </v-icon>
 </template>
+<style scoped>
+.icon {
+  padding: 10px;
+  font-size: 30px;
+  text-align: center;
+  text-decoration: none;
+  color: none;
+}
+</style>
 <script>
 export default {
   props: {
@@ -10,15 +30,16 @@ export default {
       type: String,
       default: 'button'
     },
+    link: {
+      type: String,
+      required: false,
+      default: null
+    },
     darkMode: {
       type: Boolean,
       default: true
     },
     icon: {
-      type: String,
-      default: null
-    },
-    href: {
       type: String,
       default: null
     }
