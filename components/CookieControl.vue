a9035f6daced3f58d1c88f1f1eb17a526fd052dc
Daniel Kaufmann <daniel.kaufmann96@gmx.at>
2020-01-17 23:33:10 +0100

    [BUGFIX] set default for analytics cookie

--- components/CookieControl.vue
+++ components/CookieControl.vue
@@ -61,7 +61,7 @@ export default {
       analytics:
         this.$cookies.get('optional-cookies') !== undefined
           ? this.$cookies.get('optional-cookies').activated
-          : false,
+          : true,
       settingsOpened: false
     }
   },
