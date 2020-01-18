9aba8c6bec891b649aa8af410dc0cf88b7aa49da
Daniel Kaufmann <daniel.kaufmann96@gmx.at>
2020-01-18 13:29:08 +0100

    [FEATURE] add scoped slot

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,29 +1,50 @@
 <template>
   <v-app :dark="darkModeComputed">
-    <v-container class="text-center p-container">
-      <ButtonCircleCollection>
-        <BaseIconButton
-          v-for="(navigationElement, key) in navigation"
-          :key="key"
-          :name="navigationElement.name"
-          :type="navigationElement.icon"
-          :link="navigationElement.link"
-        ></BaseIconButton>
-        <DarkModeButton
-          name="dark-mode"
-          :dark-mode="darkMode"
-          @darkModeSet="darkMode = $event"
-        ></DarkModeButton>
-        <SocialButton
-          v-for="(social, key) in socials"
-          :key="key"
-          :name="social.name"
-          :type="social.icon"
-          :link="social.link"
-        ></SocialButton>
+    <v-container class="text-center p-container my-auto" fluid>
+      <ButtonCircleCollection class="mx-auto my-5">
+        <template slot-scope="scope">
+          <BaseIconButton
+            name="home"
+            label="Home"
+            icon="mdi-home"
+            link="/"
+            class="hexagon color-home"
+            @click.native="scope.uniteHexagon"
+          >
+          </BaseIconButton>
+          <BaseIconButton
+            v-for="(navigationElement, key) in navigation"
+            :key="key"
+            :name="navigationElement.name"
+            :label="navigationElement.label"
+            :icon="`mdi-${navigationElement.icon}`"
+            :link="navigationElement.link"
+            :class="`hexagon color-${navigationElement.name}`"
+            @click.native="scope.splitHexagon"
+          ></BaseIconButton>
+          <DarkModeButton
+            name="darkmode"
+            :dark-mode="darkMode"
+            :class="`hexagon color-darkmode-${darkMode ? 'dark' : 'light'}`"
+            @darkModeSet="darkMode = $event"
+          ></DarkModeButton>
+          <SocialButton
+            v-for="(social, key) in socials"
+            :key="key"
+            :name="social.name"
+            :label="social.label"
+            :type="social.icon"
+            :link="social.link"
+            :class="`hexagon color-${social.name}`"
+            @click.native="scope.splitHexagon"
+          ></SocialButton>
+        </template>
+        <template slot="content">
+          <router-view></router-view>
+        </template>
       </ButtonCircleCollection>
     </v-container>
-    <v-footer absolute padless>
+    <v-footer padless>
       <v-card flat tile width="100%" class="text-center">
         <v-card-text>
           &copy; Daniel Kaufmann {{ new Date().getFullYear() }}
@@ -52,45 +73,59 @@ export default {
       fixed: false,
       darkMode: true,
       navigation: {
-        home: {
-          name: 'home',
-          icon: 'home',
-          link: '/home'
-        },
         about: {
           name: 'about',
-          icon: 'about',
+          label: 'About',
+          icon: 'account',
           link: '/about'
+        },
+        projects: {
+          name: 'projects',
+          label: 'Projects',
+          icon: 'iframe-outline',
+          link: '/projects'
+        },
+        contact: {
+          name: 'contact',
+          label: 'Contact',
+          icon: 'email',
+          link: '/contact'
         },
         imprint: {
           name: 'imprint',
-          icon: 'imprint',
+          label: 'Impressum',
+          icon: 'iframe-outline',
           link: '/imprint'
         }
       },
       socials: {
         github: {
           name: 'github',
+          label: 'Github',
           icon: 'github-circle',
           link: '//www.github.com/dkaufmann96'
         },
         xing: {
           name: 'xing',
+          label: 'Xing',
           icon: 'xing',
           link: '//www.xing.com/profile/Daniel_Kaufmann87'
         },
         linkedin: {
           name: 'linkedin',
+          label: 'Linkedin',
           icon: 'linkedin',
           link: 'https://www.linkedin.com/in/daniel-kaufmann-5364a1187/'
         },
         gitlab: {
           name: 'gitlab',
+          label: 'Gitlab',
           icon: 'gitlab',
           link: 'https://gitlab.com/dkaufmann96'
         },
         twitter: {
           name: 'twitter',
+          label: 'Twitter',
           icon: 'twitter',
           link: 'https://twitter.com/dkaufmann96'
         }
