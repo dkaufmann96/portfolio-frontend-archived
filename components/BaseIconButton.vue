bae79ea9dbae83b2c870e37d87973cd2102451f0
Daniel Kaufmann <daniel.kaufmann96@gmx.at>
2020-01-18 13:28:27 +0100

    [FEATURE] add nuxt routing to buttons

--- components/BaseIconButton.vue
+++ components/BaseIconButton.vue
@@ -1,12 +1,16 @@
 <template v-slot:activator>
   <a v-if="externalLink" :href="externalLink" class="icon">
-    <v-icon :name="name" @click="$emit('click')">
+    <v-icon class="showIcon" :name="name" @click="$emit('click')">
       {{ icon }}
     </v-icon>
+    <span class="showText">{{ label }}</span>
   </a>
-  <v-icon v-else class="icon" :name="name" @click="$emit('click')">
-    {{ icon }}
-  </v-icon>
+  <nuxt-link v-else :to="link" class="icon">
+    <v-icon class="showIcon" :name="name" @click="$emit('click')">
+      {{ icon }}
+    </v-icon>
+    <span class="showText">{{ label }}</span>
+  </nuxt-link>
 </template>
 <style scoped>
 .icon {
@@ -14,6 +18,9 @@
   text-align: center;
   text-decoration: none;
   color: none;
+}
+.theme--light.v-icon {
+  color: #ffffff;
 }
 </style>
 <script>
@@ -23,10 +30,14 @@ export default {
       type: String,
       default: 'button'
     },
+    label: {
+      type: String,
+      default: ''
+    },
     link: {
       type: String,
       required: false,
-      default: null
+      default: ''
     },
     externalLink: {
       type: String,
@@ -40,6 +51,10 @@ export default {
     icon: {
       type: String,
       default: null
+    },
+    showText: {
+      type: Boolean,
+      default: false
     }
   }
 }
