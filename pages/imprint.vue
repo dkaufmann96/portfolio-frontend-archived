ea36bad0dad4199c7f5ff18604635ea2c479012d
Daniel Kaufmann <daniel.kaufmann96@gmx.at>
2020-01-18 13:55:25 +0100

    [TASK] add imprint and data protection files

--- pages/imprint.vue
+++ pages/imprint.vue
@@ -1,5 +1,23 @@
 <template>
   <div>
-    imprint
+    <p>Name: Daniel Kaufmann</p>
+    <p>
+      Standort der Gewerbeberechtigung: Friesstraße 15, 3943 Schrems
+    </p>
+    <p>
+      Dienstleistungen in der automatischen Datenverarbeitung und
+      Informationstechnik
+    </p>
+    <p>
+      Kontaktdaten: Email: contact@danielkaufmann.at
+    </p>
+    <p>
+      UID-Nummer:
+    </p>
+    <p>
+      Mitgliedschaften bei der Wirtschaftskammerorganisation: Mitglied der WKÖ,
+      WKNÖ
+    </p>
+    <nuxt-link to="/data-protection">Datenschutzerklärung</nuxt-link>
   </div>
 </template>
