df5479846ea8857ec2a837af80b6fefb2a60de68
Daniel Kaufmann <daniel.kaufmann96@gmx.at>
2020-01-19 13:01:06 +0100

    [FEATURE] adjust headlines

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,13 @@
 <template>
   <v-app :dark="darkModeComputed">
     <v-container class="text-center p-container mt-5" fluid>
-      <h1>Daniel Kaufmann</h1>
-      <h2>Software Engineer in Vienna, Austria</h2>
+      <h1 class="display-1">Daniel Kaufmann</h1>
+      <h2 class="headline">Software Engineer located in Vienna, Austria</h2>
       <v-alert class="mt-5">
         Hinweis: Diese Seite ist noch in Arbeit und wird laufend erweitert.
       </v-alert>
     </v-container>
+    <CookieControl></CookieControl>
     <v-container class="text-center p-container my-auto" fluid>
       <ButtonCircleCollection class="p-container">
         <template slot-scope="scope">
@@ -59,12 +60,14 @@ import BaseIconButton from '@/components/BaseIconButton'
 
 import SocialButton from '@/components/SocialButton'
 import ButtonCircleCollection from '@/components/ButtonCircleCollection'
+import CookieControl from '@/components/CookieControl'
 
 export default {
   components: {
     BaseIconButton,
     SocialButton,
-    ButtonCircleCollection
+    ButtonCircleCollection,
+    CookieControl
   },
   data() {
     return {
