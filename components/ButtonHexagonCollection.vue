25fa456f23d50257d1ff23360d742ac3e4ebd5cb
Daniel Kaufmann <daniel.kaufmann96@gmx.at>
2020-02-16 21:21:54 +0100

    [FEATURE] simplify splitting of hexagon

--- components/ButtonHexagonCollection.vue
+++ components/ButtonHexagonCollection.vue
@@ -1,17 +1,21 @@
 <template>
-  <div>
-    <div v-if="!splitMode" class="hexagon-container">
-      <slot :splitHexagon="splitHexagon" :uniteHexagon="uniteHexagon"></slot>
-    </div>
-    <v-container
-      v-show="splitMode"
-      id="content-wrapper"
-      class="text-center p-container my-auto"
-    >
-      <div id="top" class="hexagon-container"></div>
-      <slot name="content"></slot>
-      <div id="bottom" class="hexagon-container"></div>
-    </v-container>
+  <div :class="splitMode ? 'row' : 'p-container row'">
+    <transition class="slide-fade">
+      <div
+        :class="
+          splitMode
+            ? 'hexagon-container split col-md-4 ml-2'
+            : 'hexagon-container my-auto'
+        "
+      >
+        <slot :splitHexagon="splitHexagon" :uniteHexagon="uniteHexagon"></slot>
+      </div>
+    </transition>
+    <transition class="slide-fade">
+      <div v-show="splitMode" class="col-md-6">
+        <slot name="content"></slot>
+      </div>
+    </transition>
   </div>
 </template>
 <script>
@@ -24,30 +28,10 @@ export default {
   },
   methods: {
     uniteHexagon() {
-      const top = document.getElementById('top')
-      const bottom = document.getElementById('bottom')
-      while (top.lastChild) {
-        top.removeChild(top.lastChild)
-      }
-      while (bottom.lastChild) {
-        bottom.removeChild(bottom.lastChild)
-      }
       this.splitMode = false
     },
     splitHexagon() {
       this.splitMode = true
-
-      const elements = document.getElementsByClassName('hexagon')
-      const top = document.getElementById('top')
-      const bottom = document.getElementById('bottom')
-      if (top.hasChildNodes()) {
-        return
-      }
-      for (let [index, element] of Object.entries(elements)) {
-        index < elements.length / 2
-          ? top.appendChild(element)
-          : bottom.appendChild(element)
-      }
     }
   }
 }
